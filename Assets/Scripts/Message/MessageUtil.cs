25f47284774912a4194ab48c56239e85540a42ab
hwkim <hwpeterkim@gmail.com>
2014-03-06 16:57:37 +0900

    initial commit.

---
+++ Assets/Scripts/Message/MessageUtil.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+/// <summary>
+/// !! Decrpecated !!
+/// 
+/// http://www.codingvision.net/security/c-php-compatible-encryption-aes256
+/// 
+/// </summary>
+public class MessageUtil 
+{
+	public static string EncryptMessage(byte[] text, string key)
+	{
+		RijndaelManaged aes = new RijndaelManaged();
+		aes.KeySize = 256;  
+		aes.BlockSize = 256;
+		aes.Padding = PaddingMode.Zeros;
+		aes.Mode = CipherMode.CBC;
+		
+		aes.Key = Encoding.Default.GetBytes(key);
+		aes.GenerateIV();  
+		
+		string IV = ("-[--IV-[-" + Encoding.Default.GetString(aes.IV));
+		
+		ICryptoTransform AESEncrypt = aes.CreateEncryptor(aes.Key, aes.IV);
+		byte[] buffer = text;
+		
+		return Convert.ToBase64String(Encoding.Default.GetBytes(Encoding.Default.GetString(AESEncrypt.TransformFinalBlock(buffer, 0, buffer.Length)) + IV));
+		
+	}
+
+	public static string DecryptMessage(string text, string key)
+	{
+		RijndaelManaged aes = new RijndaelManaged();
+		aes.KeySize = 256;
+		aes.BlockSize = 256;
+		aes.Padding = PaddingMode.Zeros;
+		aes.Mode = CipherMode.CBC;
+		
+		aes.Key = Encoding.Default.GetBytes(key);
+		
+		text = Encoding.Default.GetString(Convert.FromBase64String(text));
+		
+		string IV = text;
+		IV = IV.Substring(IV.IndexOf("-[--IV-[-") + 9);
+		text = text.Replace("-[--IV-[-" + IV, "");
+		
+		text = Convert.ToBase64String(Encoding.Default.GetBytes(text));
+		aes.IV = Encoding.Default.GetBytes(IV);
+		
+		ICryptoTransform AESDecrypt = aes.CreateDecryptor(aes.Key, aes.IV);
+		byte[] buffer = Convert.FromBase64String(text);
+		
+		return Encoding.Default.GetString(AESDecrypt.TransformFinalBlock(buffer, 0, buffer.Length));
+	}
+
+}
---
+++ Assets/Scripts/Test/AES256CBCTest.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using CoreUtils.Utilities;
+
+
+/// <summary>
+/// AES-256-CBC Test
+/// </summary>
+public class AES256CBCTest : MonoBehaviour 
+{
+	
+	void Start () 
+	{
+		// does not correctly work as it is expected!
+		//TestSuite_MessageUtil();
+
+		// works fine!
+		TestSuite_CoreUtils ();
+
+	}
+
+	/// <summary>
+	/// 
+	/// Found at:
+	/// 	http://stackoverflow.com/questions/18502375/aes256-encryption-decryption-in-both-nodejs-and-c-sharp-net
+	/// 
+	/// </summary>
+	void TestSuite_CoreUtils()
+	{
+		string message = "Hello, world";
+		string key = "이왕만들거제대로재미있게만들고서비스하자쪽팔리지말고_~!!";
+		//string key = "This is my password.";
+		
+		Debug.Log ("Original message: " + message);
+		
+		string encryptedMsg = CryptoUtility.Encrypt (message, key);
+		
+		Debug.Log ("Ectripted Message: " + encryptedMsg);
+		
+		string decryptedMsg = CryptoUtility.Decrypt (encryptedMsg, key);
+		
+		Debug.Log("Decrypted Message: " + decryptedMsg);
+	}
+
+	/// <summary>
+	/// 
+	/// Found at:
+	/// 	http://www.codingvision.net/security/c-php-compatible-encryption-aes256
+	/// 
+	/// </summary>
+	void TestSuite_MessageUtil()
+	{
+		string message = "Hello, world";
+		//string key = "이왕만들거제대로재미있게만들고서비스하자쪽팔리지말고_~!!";
+		string key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ012345";
+		
+		Debug.Log ("orignal message: " + message);
+		
+		string encriptedMessage = MessageUtil.EncryptMessage( GetBytes(message), key);
+		
+		string descriptedMesssage = MessageUtil.DecryptMessage (encriptedMessage, key);
+		
+		Debug.Log ("descripted messsage: " + descriptedMesssage);
+	}
+
+	// deprecated.
+	static byte[] GetBytes(string str)
+	{
+		byte[] bytes = new byte[str.Length * sizeof(char)];
+		System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+		return bytes;
+	}
+
+}
